3a1de1b1d5998d576d3ebffbf04d730528df93c7
User <user@Users-MacBook-Pro.local>
2019-06-15 18:28:55 -0700

    Add stop words to naive try

--- initial.py
+++ initial.py
@@ -1,8 +1,10 @@
 test_data = """White tigers live mostly in India
                Wild lions live mostly in Africa"""
-
+stop_words = set(['in', 'the', 'and', 'for']) # etc etc
 term_frequency_hash = {}
 def add_tf(word):
+    if word in stop_words:
+        return
     if word in term_frequency_hash:
         term_frequency_hash[word] += 1
     else:
